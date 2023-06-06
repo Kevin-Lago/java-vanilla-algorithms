b14413313b85184069e74b5ed5da58dfbe8a0b48
Kevin Lago <kevinthelago@gmail.com>
2023-06-05 20:15:59 -0400

    Initial Commit

---
+++ src/SimpleFibonacciGenerator.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleFibonacciGenerator {
+    public static void main(String[] args) {
+        int fn = simpleFibonacciSeries(10);
+        System.out.println(fn);
+    }
+
+    public static int simpleFibonacciSeries(int n) {
+        for (int i = n; i > 0; i--) {
+            if (i < 2) {
+                return 1;
+            }
+            return simpleFibonacciSeries(i - 1) + simpleFibonacciSeries(i - 2);
+        }
+        return 0;
+    }
+
+    public static int memoizedFibonacciSeries(int n) {
+        List<Integer> memos = new ArrayList<>();
+
+        for (int i = n; i > 0; i--) {
+            if () {
+
+            }
+        }
+    }
+}
